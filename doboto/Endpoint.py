1e573998f9082595516d58f7d75a04817f6e21af
bmildren <ben.mildren@gmail.com>
2016-10-20 19:29:34 -0400

    Initial sshkey methods using endpoint make_request

--- doboto/Endpoint.py
+++ doboto/Endpoint.py
@@ -1,5 +1,8 @@
 """This holds the Endpoint class."""
 
+import requests
+import json
+
 
 class Endpoint(object):
     """Base class for interacting with an endpoint of the DO API."""
@@ -7,3 +10,43 @@ class Endpoint(object):
     def __init__(self, token):
         """Take token and sets its token for API authorization."""
         self.token = token
+
+    def process_response(self, response):
+        """Process response."""
+        if response.status_code == 204:
+            return {'status': response.status_code}
+        else:
+            return response.json()
+
+    def make_request(self, request_url, request_method='GET', attribs=None):
+        """Make request to DO API."""
+        headers = {'Authorization': "Bearer %s" % self.token,
+                   'Content-Type': 'application/json'}
+
+        if request_method == 'POST':
+            resp = requests.post(request_url,
+                                 data=json.dumps(attribs),
+                                 headers=headers,
+                                 timeout=60)
+            json_response = self.process_response(resp)
+        elif request_method == 'DELETE':
+            resp = requests.delete(request_url,
+                                   data=json.dumps(attribs),
+                                   headers=headers,
+                                   timeout=60)
+            json_response = self.process_response(resp)
+        elif request_method == 'PUT':
+            resp = requests.put(request_url,
+                                headers=headers,
+                                params=attribs,
+                                timeout=60)
+            json_response = resp.json()
+        elif request_method == 'GET':
+            resp = requests.get(request_url,
+                                headers=headers,
+                                params=attribs,
+                                timeout=60)
+            print resp
+            json_response = resp.json()
+
+        return json_response
--- doboto/SSHKey.py
+++ doboto/SSHKey.py
@@ -9,4 +9,27 @@ class SSHKey(Endpoint):
     def __init__(self, url, token):
         """Take token and sets its URI for SSH key interaction."""
         super(SSHKey, self).__init__(token)
-        self.uri = "%s/ssh_keys" % url
+        self.uri = "%s/account/keys" % url
+
+    def create(self, name, public_key):
+        """Create SSH Key"""
+        attribs = {'name': name,
+                   'public_key': public_key}
+
+        return self.make_request(self.uri, 'POST', attribs)
+
+    def list(self):
+        """list SSH Keys"""
+
+        return self.make_request(self.uri)
+
+    def retrieve(self, ssh_id):
+        """Retrieve SSH Key"""
+
+
+    def update(self, ssh_id):
+        """Update SSH Key"""
+
+
+    def destroy(self, ssh_id):
+        """Destroy SSH Key"""
