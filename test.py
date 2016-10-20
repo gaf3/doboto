53e3d40c9f84fd2a9d6e5b941642e1070d97b26d
bmildren <ben.mildren@gmail.com>
2016-10-20 19:33:21 -0400

    Removed quick and dirty test

--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-from doboto.DO import DO
-
-do = DO(url="https://api.digitalocean.com/v2/",token="e3cc092ee48e428e54067b5ebda86b0d7592a242fd43886d4ba5cc06f603eda2")
-
-ssh_keys = do.ssh_key.list()
-
-print ssh_keys
