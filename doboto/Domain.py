3ec8e16d6fabfa4c31d73d4e89448ada12000978
Gaffer Fitch <gfitch@digitalocean.com>
2017-01-31 15:14:24 -0500

    Completed Domain

--- doboto/Domain.py
+++ doboto/Domain.py
@@ -14,50 +14,52 @@ class Domain(Endpoint):
         super(Domain, self).__init__(token)
         self.uri = "{}/domains".format(url)
 
+    def create(self, name, ip_address):
+        """Create a domain based off of parameters"""
+
+        attribs = {
+            "name": name,
+            "ip_address": ip_address
+        }
+
+        return self.make_request(self.uri, 'POST', attribs=attribs)
+
     def list(self):
         """
         list all domains
         """
         return self.make_request(self.uri)
 
-    def info(self, domain_name):
+    def info(self, name):
         """
         Retrieve domain information
         """
-        uri = "{}/{}".format(self.uri, domain_name)
+        uri = "{}/{}".format(self.uri, name)
         return self.make_request(uri)
 
-    def destroy(self, domain_name):
+    def destroy(self, name):
         """
         Destroy a domain
         """
 
-        uri = "{}/{}".format(self.uri, domain_name)
+        uri = "{}/{}".format(self.uri, name)
         return self.make_request(uri, 'DELETE')
 
-    def create(self, attribs=None):
-        """Create a domain based off of parameters"""
-
-        if attribs is None:
-            raise ValueError("Must supply a domain name and address")
-
-        return self.make_request(self.uri, 'POST', attribs=attribs)
-
-    def list_records(self, domain_name):
+    def records(self, name):
         """
         Retrieve all domain records
         """
-        uri = "{}/{}/records".format(self.uri, domain_name)
+        uri = "{}/{}/records".format(self.uri, name)
         return self.make_request(uri)
 
-    def get_record(self, domain_name, record_id):
+    def record_info(self, name, record_id):
         """
         Retrieve specific domain record
         """
-        uri = "{}/{}/records/{}".format(self.uri, domain_name, record_id)
+        uri = "{}/{}/records/{}".format(self.uri, name, record_id)
         return self.make_request(uri)
 
-    def edit_record(self, domain_name, record_id, attribs=None):
+    def record_update(self, name, record_id, attribs=None):
         """
         Update specific domain record
         """
@@ -65,13 +67,13 @@ class Domain(Endpoint):
         if attribs is None:
             raise ValueError("Must supply a data to edit")
 
-        uri = "{}/{}/records/{}".format(self.uri, domain_name, record_id)
+        uri = "{}/{}/records/{}".format(self.uri, name, record_id)
         return self.make_request(uri, 'PUT', attribs=attribs)
 
-    def delete_record(self, domain_name, record_id):
+    def record_destroy(self, name, record_id):
         """
         Delete specific domain record
         """
 
-        uri = "{}/{}/records/{}".format(self.uri, domain_name, record_id)
+        uri = "{}/{}/records/{}".format(self.uri, name, record_id)
         return self.make_request(uri, 'DELETE')
