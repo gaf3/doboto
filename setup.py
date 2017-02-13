5033b4cf8495183c3448f2336be550e06978b883
Gaffer Fitch <gfitch@digitalocean.com>
2017-02-13 13:05:35 -0500

    Got into pip

--- setup.py
+++ setup.py
@@ -3,19 +3,16 @@
 from distutils.core import setup
 import os
 
-our_dir = os.path.abspath(os.path.dirname(__file__))
-__version__ = open(our_dir + "/VERSION", "r").readline().strip()
-
 setup(
     name='doboto',
-    version=__version__,
-    description="doboto",
+    version="0.3.2",
+    description="BOTO-like library for interacting with the Digital Ocean API",
     packages=['doboto'],
     long_description="BOTO-like library for interacting with the Digital Ocean API",
     author="Digital Ocean Data Team",
     author_email="swe-data@do.co",
     classifiers=[
-        'Programming Language :: Python :: 2'
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7'
     ],
 )
